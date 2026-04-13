9ceb2c0505af2e2ef5b7c3c768359eaac65f299b
Savinien Petitjean <savinien.petitjean@gmail.com>
2026-04-13 15:32:49 +0200

    Add CI

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
-void main(void) {
-    printf("Hello World!");
-    
-    while(1) {
-        wait_vbl_done();
-    }
+int main(void) {
+  printf("Hello World!");
+
+  while (1) {
+    wait_vbl_done();
+  }
 }
