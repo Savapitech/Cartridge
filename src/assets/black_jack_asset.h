e2ced9090f29218f33fa4d91316dcabf333e505e
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-05-02 16:38:33 +0200

    Add Black Jack Asset

---
+++ src/assets/black_jack_asset.h
@@ -0,0 +1,10 @@
+#ifndef ASSETS_BLACK_JACK_H
+#define ASSETS_BLACK_JACK_H
+
+#include <stdint.h>
+
+extern const uint8_t coeurs_tile[16];
+extern const uint8_t pique_tile[16];
+extern const uint8_t trefle_tile[16];
+extern const uint8_t carreau_tile[16];
+#endif
